46d2e89bf47d3cd23cf86a7780d97217bb5c681f
SunZar <121431240+SunZar@users.noreply.github.com>
2025-01-31 22:42:59 +0800

    feat: Написана вся разметка приложения в XML
    Задание 4. Разработка интерфейса - Выполнено

---
+++ app/src/main/java/ru/sicampus/bootcamp2025/MainActivity.java
@@ -0,0 +1,18 @@
+package ru.sicampus.bootcamp2025;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import ru.sicampus.bootcamp2025.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
