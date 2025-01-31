357df31758f59dc8eaddac3774aac51aff1cef3f
Milkkooo5 <ananbekh@icloud.com>
2025-01-31 05:49:23 +0300

    Profile Screen

--- app/src/main/java/ru/sicampus/bootcamp2025/MainActivity.kt
+++ app/src/main/java/ru/sicampus/bootcamp2025/MainActivity.kt
@@ -1,20 +1,37 @@
 package ru.sicampus.bootcamp2025
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import ru.sicampus.bootcamp2025.ui.navigation.screens.EditProfileScreen
+import ru.sicampus.bootcamp2025.ui.navigation.screens.ProfileScreen
+import ru.sicampus.bootcamp2025.ui.navigation.screens.Screen
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContent {
+            val navController = rememberNavController()
+            NavHost(
+                navController = navController,
+                startDestination = Screen.Profile.route
+            ) {
+                composable(Screen.Profile.route) {
+                    ProfileScreen(
+                        onEditClick = { navController.navigate(Screen.EditProfile.route) } ,
+                        onCancel = { navController.navigate(Screen.EditProfile.route) }
+                    )
+                }
+                composable(Screen.EditProfile.route) {
+                    EditProfileScreen(
+                        onCancel = { navController.navigate(Screen.Profile.route) },
+                        onSave = { navController.popBackStack() },
+                    )
+                }
+            }
         }
     }
 }
