06aab3b8a6b2d75e85cee39b0c5e41960070b3ff
DmitryKaverznev <dmitry.kaverznev@gmail.com>
2025-01-31 17:06:49 +0300

    style: Главный экран сделан

--- app/src/main/java/ru/sicampus/bootcamp2025/ProfileFragment.kt
+++ app/src/main/java/ru/sicampus/bootcamp2025/ProfileFragment.kt
@@ -13,6 +13,6 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 }
