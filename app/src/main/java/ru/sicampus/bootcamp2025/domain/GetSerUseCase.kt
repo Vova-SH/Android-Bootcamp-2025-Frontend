13862c6a4a8bacff7d658f2dc6b03409f54cb63e
vladim <89231915270v@gmail.com>
2025-02-08 11:21:07 +0300

    Bug fixes

--- app/src/main/java/ru/sicampus/bootcamp2025/domain/GetSerUseCase.kt
+++
@@ -1,9 +0,0 @@
-package ru.sicampus.bootcamp2025.domain
-
-class GetSerUseCase(private val repo: UserRepo,
-) {
-    operator suspend fun invoke(
-        pageNumb: Int,
-        pageSize: Int,
-    ) = repo.getUsers(pageNumb, pageSize)
-}
